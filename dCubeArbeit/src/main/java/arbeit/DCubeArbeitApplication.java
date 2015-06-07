14f845c28134e9c77c718eb569dd949e73690944
JinHyung <haneu89@naver.com>
2015-06-07 17:19:30 +0900

    init

---
+++ dCubeArbeit/src/main/java/arbeit/DCubeArbeitApplication.java
@@ -0,0 +1,12 @@
+package arbeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DCubeArbeitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DCubeArbeitApplication.class, args);
+    }
+}
---
+++ dCubeArbeit/src/main/java/arbeit/ServletInitializer.java
@@ -0,0 +1,13 @@
+package arbeit;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DCubeArbeitApplication.class);
+	}
+
+}
